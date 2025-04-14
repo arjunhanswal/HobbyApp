fd8791da375b59ed549ba0843dcd78eb1350b281
dev-2024 <test@gmail.com>
2025-04-14 10:02:21 +0530

    initial commit - Create project

---
+++ HobbyApp/HobbyApp/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  HobbyApp
+//
+//  Created by Kavita on 14/04/25.
+//  Copyright © 2025 test. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
