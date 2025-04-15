09b1b0f856e50c6e123d77fa1ba19e0101557e2e
arjun <arjunhanswal@gmail.com>
2025-04-15 10:10:03 +0530

    feat: Added custom color palette.

---
+++ HobbyApp/HobbyApp/Utility/Theme/Color+Extension.swift
@@ -0,0 +1,25 @@
+//
+//  Color+Extension.swift
+//  HobbyApp
+//
+//  Created by Kavita on 15/04/25.
+//  Copyright © 2025 test. All rights reserved.
+//
+
+import SwiftUI
+
+extension Color {
+  //Primary Color
+  static let primaryBlue = Color("PrimaryBlue") // #4A90E2
+  static let primaryGreen = Color("PrimaryGreen") // #7ED321
+  static let primaryWhite = Color("PrimaryWhite") // #FFFFFF
+  
+  //Secondary Color
+  static let darkGray = Color("darkGray") // #4A4A4A
+  static let lightGray = Color("lightGray") // #F1F1F1
+  static let accentYellow = Color("accentYellow") // #F8E71C
+  
+  //Text Color
+  static let primaryText = Color("PrimaryText") // #4A4A4A
+  static let secondaryText = Color("SecondaryText") // #9B9B9B
+}
